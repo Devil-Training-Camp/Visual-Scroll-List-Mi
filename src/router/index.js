76085a3002371038e03f89200ce73e25b3daffab
MizhiGao <>
2023-06-12 00:07:15 +0800

    feat: 基础配置&固定高度虚拟列表

---
+++ src/router/index.js
@@ -0,0 +1,35 @@
+import Vue from "vue";
+import VueRouter from "vue-router";
+Vue.use(VueRouter);
+
+const routes = [
+  {
+    path: "/",
+    redirect: { name: "FixedHeight" },
+    component: { render: (h) => h("router-view") },
+    children: [
+      {
+        path: "fixed",
+        name: "FixedHeight",
+        component: () =>
+          import(/* webpackChunkName:'fixed'*/ "@/example/index.vue"),
+      },
+      // {
+      //   path: "/dynamic",
+      //   name: "Dynamic",
+      //   component: () =>
+      //     import(
+      //       /* webpackChunkName: "Dynamic" */ "@/components/DynamicScroller.vue"
+      //     ),
+      // },
+    ],
+  },
+];
+
+const router = new VueRouter({
+  mode: "history",
+  base: process.env.BASE_URL,
+  routes,
+});
+
+export default router;
